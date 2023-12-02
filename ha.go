f8cad00d1e0b733eda174339a948844e28deb8d6
songbowen <1303535630@qq.com>
2023-12-03 00:22:02 +0800

    add: config module
    read config from file.

--- ha.go
+++ ha.go
@@ -1,5 +1,20 @@
 package HA
 
-func main() {
+import (
+	"HA/pkg/config"
+	"flag"
+	"fmt"
+)
 
+var (
+	raftConf = flag.String("raft_conf", "conf/raft.conf.json", "raft config filepath")
+)
+
+func main() {
+	// 1. init config
+	flag.Parse()
+	raftConfig := config.NewConfiguration(*raftConf)
+	fmt.Println(raftConfig)
+
+	// 2. run raft
 }
---
+++ pkg/config/config_manager.go
@@ -0,0 +1,8 @@
+package config
+
+type Configuration struct {
+}
+
+func NewConfiguration(fileName string) Configuration {
+	return Configuration{}
+}
